11a8990cce3ae0f0c164b4838e1500b9e85a82ae
LavanyaM134 <lavanyamanjunath134@gmail.com>
2024-02-07 23:21:35 +0530

    Added scrolling  text and images in home page

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel} from 'react-bootstrap'
+import {Carousel, Row, Col, Button} from 'react-bootstrap';
 
 const HomeScreen =() => {
 
@@ -53,6 +53,33 @@ const HomeScreen =() => {
                     />
                 </Carousel.Item>
             </Carousel>
+            <br/>
+            <div>
+                <Row>
+                    <Col>
+                        <h3><a href="#">Vision</a></h3>
+                        <p className='p-style'>To become an outstanding Technological University at the cutting edge of Science and Technology that produces world class Knowledge-delivery, Research, Extension and Leadership in Technology innovation for Industry and Society.</p>
+                        <p className='p-style'>To become an outstanding Technological University at the cutting edge of Science and Technology that produces world class Knowledge-delivery, Research, Extension and Leadership in Technology innovation for Industry and Society.</p>
+                        <Button variant='warning'>Read more</Button>
+                    </Col>
+                    <Col>
+                    <h3><a href="#">Mission</a></h3>
+                        <p className='p-style'>To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                        <p className='p-style'>To plan the development of technical education, to establish value-based and need-based education and training in engineering and technology, with a view to generate qualified and competent manpower, responsive to technological and societal needs.</p>
+                        <Button variant='warning'>Read more</Button>
+                    </Col>
+                    <Col>
+                        <h3><a href="#">Mandate</a></h3>
+                        <p className='p-style'>The Visvesvaraya Technological University has been established by the Government of the Karnataka in order to Promote planned and sustainable development of technical education consistent with state and national policies.</p>
+                        <p className='p-style'>*Design need based programs that ensure adequate human resource of the right kind.</p>
+                        <p className='p-style'>*Establish objective evaluation and certification systems for students,programs and institutions.</p>
+                        <p className='p-style'>*Collaborate with national and international institutions R&D establishments organizations like AICTE, MHRD, UGC, etc.industry and user agencies.</p>
+                        <Button variant='warning'>Read more</Button>
+
+                    </Col>
+                </Row>
+            </div>
+
         </div>
 
         </>
